d79a37ad735febc787fcb5e0b87f7d597562f51a
Belgaux <oscarbelgau@gmail.com>
2017-11-05 16:09:09 +0100

    Move some old cpp utils.

--- python/gemm/sandbox.py
+++ python/gemm/sandbox.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-W = np.ndarray(shape=(1, 32), dtype=int)
-W.fill(-1)
+W = np.ndarray(shape=(8, 32), dtype=int)
+W.fill(1)
 
-A = np.ndarray(shape=(32, 1), dtype=int)
-A.fill(-1)
+A = np.ndarray(shape=(32, 4), dtype=int)
+A.fill(1)
 print(W, A)
 
 print(np.dot(W,A))
